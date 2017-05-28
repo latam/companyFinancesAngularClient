f99322e540bff1fba7e7b21eaf2d0bf369582054
Mateusz <m_lata@op.pl>
2017-05-28 11:55:14 +0200

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {DatePipe} from '@angular/common';
+
 import { AppComponent } from './app.component';
 import {RegistrationComponent} from "./account/registration/registration.component";
 import {LoginComponent} from "./account/login/login.component";
-import {AuthenticationService} from "./service/authentication.service";
 import {HomeComponent} from "./home/home.component";
-import {CanActivateAuthGuard} from "./account/login/authguard";
-import {CompanyService} from "./service/company.service";
 import {CompanyComponent} from "./account/company/company.component";
 import {OperationsComponent} from "./operations/operations.component";
-import {OperationsService} from "./service/operations.service";
-import {DatePipe} from "@angular/common";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap"
 import {OperationModalComponent} from "./operations/operation.modal.component";
-import {VatRegistryService} from "./service/vatregistry.service";
 import {VatRegistryComponent} from "./vatregistry/vatregistry.component";
-import {KpirRegistryService} from "./service/kpirregistry.service";
 import {KpirRegistryComponent} from "./kpirregistry/kpirregistry.component";
 import {CompanyModalComponent} from "./account/company/company.modal.component";
 
 import {DateFilterComponent} from "./common/filter/date.filter.component";
-import {RouterModule, Routes} from "@angular/router";
+
+import {AuthenticationService} from "./service/authentication.service";
+import {CompanyService} from "./service/company.service";
+import {OperationsService} from "./service/operations.service";
+import {VatRegistryService} from "./service/vatregistry.service";
+import {KpirRegistryService} from "./service/kpirregistry.service";
+
+import {CanActivateAuthGuard} from "./account/login/authguard";
 
 const appRoutes: Routes = [
   {
@@ -77,8 +81,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [
     AuthenticationService,
@@ -89,11 +93,11 @@ const appRoutes: Routes = [
     CanActivateAuthGuard,
     DatePipe
   ],
-  bootstrap: [AppComponent],
   entryComponents: [
     OperationModalComponent,
     CompanyModalComponent,
     DateFilterComponent
   ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
